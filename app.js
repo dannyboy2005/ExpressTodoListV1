954061402d34873a1c606d068b5a6304ad58c270
Daniel Boffey <dboffey70@googlemail.com>
2020-10-14 14:29:41 +0100

    Added EJS

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get("/", function(req,res) {
+    let day = "";
+    let today = new Date();
+    let currentDay = today.getDay()
+
+    if(currentDay === 0) {
+        day = "Sunday"
+        res.render("lists", {kindOfDay: day})
+    } else if (currentDay === 1) {
+        day = "Monday"
+        res.render("lists", {kindOfDay: day})
+    } else if (currentDay === 2) {
+        day = "Tuesday"
+        res.render("lists", {kindOfDay: day})
+    } else if (currentDay === 3) {
+        day = "Wednesday"
+        res.render("lists", {kindOfDay: day})
+    } else if (currentDay === 4) {
+        day = "Thursday"
+        res.render("lists", {kindOfDay: day})
+    } else if (currentDay === 5) {
+        day = "Friday"
+        res.render("lists", {kindOfDay: day})
+    } else {
+        day = "Saturday"
+        res.render("lists", {kindOfDay: day})
+
+    }
+
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+})
