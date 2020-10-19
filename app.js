745f27ba0f8a0cd6ceab43048044b763e8e81bb0
Daniel Boffey <dboffey70@googlemail.com>
2020-10-19 15:16:53 +0100

    completed todolist V1

--- app.js
+++ app.js
@@ -3,38 +3,47 @@ const bodyParser = require("body-parser")
 
 const app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"))
+const date = require(__dirname + "/date.js")
 
-// Get the day
+
+// Global Vars
+const items = [];
+const workItems = [];
+
+// Get pathway
 app.get("/", function(req,res) {
-    let day = "";
-    let today = new Date();
-    let currentDay = today.getDay()
+    
+let day = date.getDate();
 
-    if(currentDay === 0) {
-        day = "Sunday"
-        res.render("lists", {kindOfDay: day})
-    } else if (currentDay === 1) {
-        day = "Monday"
-        res.render("lists", {kindOfDay: day})
-    } else if (currentDay === 2) {
-        day = "Tuesday"
-        res.render("lists", {kindOfDay: day})
-    } else if (currentDay === 3) {
-        day = "Wednesday"
-        res.render("lists", {kindOfDay: day})
-    } else if (currentDay === 4) {
-        day = "Thursday"
-        res.render("lists", {kindOfDay: day})
-    } else if (currentDay === 5) {
-        day = "Friday"
-        res.render("lists", {kindOfDay: day})
-    } else {
-        day = "Saturday"
-        res.render("lists", {kindOfDay: day})
-
-    }
-
+    res.render("lists", {
+        listTitle: day, newListItems: items
+    });
 });
+
+
+// Post from the form to add new LI
+app.post("/", function(req, res) {
+   let item = req.body.newItem;
+
+   if(req.body.list === "Work") {
+       workItems.push(item);
+       res.redirect("/work")
+   } else {
+    items.push(item);
+
+    res.redirect("/");
+   }
+})
+
+app.get("/work", function(req, res) {
+    res.render("lists", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function (req, res) {
+    res.render("about")
+})
 
 app.listen(3000, function() {
     console.log("Server started on port 3000");
